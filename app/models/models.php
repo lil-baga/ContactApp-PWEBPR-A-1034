c037fc6086f73d9ec0f4d7aa01a0c1bc10478bfb
AkmalIhabSyauqi <akmalihab@gmail.com>
2024-04-07 12:01:13 +0700

    commit fix

---
+++ app/models/models.php
@@ -0,0 +1,16 @@
+<?php
+
+Class Contact{ 
+    static function select(){
+  
+    }
+    static function insert(){
+  
+    }
+    static function update(){
+  
+    }
+    static function delete(){
+  
+    }
+  }
